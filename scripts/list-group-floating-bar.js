538e46d304b68c4161c8cca0f218c290835942d9
AtilioRausch <atilio.rausch@uner.edu.ar>
2023-02-17 10:34:05 -0300

    Responsive navBar and footer. Scroll bar added.

---
+++ scripts/list-group-floating-bar.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".list-group-item");
+const sections = document.querySelectorAll(".section");
+
+function highlightNav() {
+  let scrollPos = window.scrollY + window.innerHeight * 0.33;
+
+  sections.forEach((section, index) => {
+    if (scrollPos >= section.offsetTop) {
+      items.forEach((item) => item.classList.remove("active"));
+      items[index].classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", highlightNav);
